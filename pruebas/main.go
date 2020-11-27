60ffcae048a8ef01919917a6f19d25e9a6bdeea8
César <c.caycho46@gmail.com>
2020-11-27 17:32:49 -0500

    inicio de pruebas xml

--- pruebas/main.go
+++ pruebas/main.go
@@ -2,42 +2,13 @@ package main
 
 import (
 	"encoding/xml"
-	"fmt"
-	"io/ioutil"
-	"log"
-	"os"
 )
 
-type user struct {
-	Username string
-	Password string
+type users struct {
+	XMLName xml.Name `xml:"users"`
+	numeros int
 }
 
 func main() {
 
-	//el contenido de databases.xml esta creado por un marshal y de parametro un slice de user
-
-	archivo, err := os.OpenFile("databases.xml", os.O_RDWR, 0644)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer archivo.Close()
-
-	contenido, err := ioutil.ReadAll(archivo)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	users := []user{}
-
-	err = xml.Unmarshal(contenido, &users)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println(users)
 }
